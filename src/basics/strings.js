7b8809c4cb8e0dde26da20a0decc92d1d1dabb20
igortrajic <iggetrajic@gmail.com>
2025-10-06 10:44:35 +0200

    feat: completed funiction strings

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -7,5 +7,32 @@
  * @return {string} the resulting string, with all needle words transformed to newWord
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
-  // Write your code here
+  if (typeof haystack !== "string") {
+    throw new Error("Unsupported type")
+  }
+  if (typeof needle !== "string") {
+    throw new Error("Unsupported type")
+  }
+  if (typeof newWord !== "string") {
+    throw new Error("Unsupported type")
+  }
+  const regex = new RegExp(needle, "gi"); 
+
+  return haystack.replace(regex, (match) => {
+    const reducedNewWord = newWord.slice(0, match.length);
+
+    let result = "";
+    for (let i = 0; i < reducedNewWord.length; i++) {
+      const originalChar = match[i];
+      const newChar = reducedNewWord[i];
+
+      if (originalChar === originalChar.toUpperCase()) {
+        result += newChar.toUpperCase();
+      } else {
+        result += newChar.toLowerCase();
+      }
+    }
+
+    return result;
+  });
 }
